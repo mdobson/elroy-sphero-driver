5dac1ee6274d59fd8b56eed9831bdb441ac0082a
Matt Dobson <mdobson4@gmail.com>
2014-04-04 18:04:24 -0400

    Changing casing of sphero type

--- sphero_driver.js
+++ sphero_driver.js
@@ -1,6 +1,6 @@
 var SpheroDriver = module.exports = function(sphero) {
   this.name = 'Matts Sphero';
-  this.type = 'Sphero';
+  this.type = 'sphero';
   this.sphero = sphero;
   this.state = 'online';
   this.color = 'none';
@@ -21,15 +21,8 @@ SpheroDriver.prototype.setColor = function(rgb, cb) {
   this.color = rgb;
 };
 
-SpheroDriver.prototype.move = function(direction, time, cb) {
-  console.log(arguments);
-  if(direction === 'forward') {
-    this.sphero.forward(time);
-  } else if( direction === 'left' ) {
-    this.sphero.left().forward(time);
-  } else if(direction === 'right' ) {
-    this.sphero.right().forward(time);
-  }
+SpheroDriver.prototype.move = function(time, cb) {
+  this.sphero.forward(time);
 };
 
 SpheroDriver.prototype.turnLeft = function(cb) {
