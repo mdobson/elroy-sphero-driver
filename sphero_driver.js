3b8f01e0870ed8960b46eca483d0c4068fd3c412
Matt Dobson <mdobson4@gmail.com>
2014-04-06 09:49:49 -0400

    Added better color logic for the lib.

--- sphero_driver.js
+++ sphero_driver.js
@@ -4,25 +4,40 @@ var SpheroDriver = module.exports = function(sphero) {
   this.sphero = sphero;
   this.state = 'online';
   this.color = 'none';
+  this.colors = {
+    'black': 0x000000,
+    'blue': 0x0000ff,
+    'green': 0x00ff00,
+    'orange': 0xff4500,
+    'pink': 0xff1444,
+    'purple': 0xff00ff,
+    'red': 0xff0000,
+    'white': 0xffffff,
+    'yellow': 0xffff00
+  };
 };
 
 SpheroDriver.prototype.init = function(config) {
   config
     .when('online', { allow: ['set-color','move'] })
     .when('moving', { allow: [] })
-    .map('set-color', this.setColor, [{name: 'rgb', type: 'text'}])
+    .map('set-color', this.setColor, [{name: 'color', type: 'text'}])
     .map('move', this.move, [{name: 'time', type:'number'}])
     .map('left', this.turnLeft)
     .map('right', this.turnRight);
 };
 
-SpheroDriver.prototype.setColor = function(rgb, cb) {
-  this.sphero.setColor(rgb);
-  this.color = rgb;
+SpheroDriver.prototype.setColor = function(color, cb) {
+  if(color in this.colors) {
+    this.sphero.setColor(rgb);
+    this.color = rgb;
+  }
+  cb();
 };
 
 SpheroDriver.prototype.move = function(time, cb) {
   this.sphero.forward(time);
+  cb();
 };
 
 SpheroDriver.prototype.turnLeft = function(cb) {
